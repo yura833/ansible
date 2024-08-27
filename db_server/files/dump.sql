b57265a464ffa58d7a1bf0e46dfecf1b2ccd5269
Yura <you@example.com>
2024-08-27 10:54:10 +0400

    New code

---
+++ db_server/files/dump.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS test (
+	message varchar(255) NOT NULL
+) ENGINE=MYISAM DEFAULT CHARSET=utf8;
+INSERT INTO test(message) VALUES('Ansible to Do List');
+INSERT INTO test(message) VALUES('Get Ready');
+INSERT INTO test(message) Values('Ansible is fun')
