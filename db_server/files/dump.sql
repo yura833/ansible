88563cbf6ae347cb6eceaf92a2734c5adece257b
Yura <you@example.com>
2024-10-02 09:56:04 +0400

    added ansible config for k8s

--- db_server/files/dump.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS test (
-	message varchar(255) NOT NULL
-) ENGINE=MYISAM DEFAULT CHARSET=utf8;
-INSERT INTO test(message) VALUES('Ansible to Do List');
-INSERT INTO test(message) VALUES('Get Ready');
-INSERT INTO test(message) Values('Ansible is fun')
